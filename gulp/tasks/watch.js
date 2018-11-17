3c5ad0671084e6ab401e6f80d6008cc75a1426d6
0kaypi <25729441+0KayPi@users.noreply.github.com>
2018-11-17 16:05:54 +0100

    Completed desktop and mobile header styles; Updated Babel.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,27 +1,36 @@
-var gulp = require("gulp"),
-watch = require("gulp-watch"),
-browserSync = require("browser-sync").create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
 gulp.task("watch", function() {
-	
-	browserSync.init({
-		browser: "C:\\Program Files\\Firefox Developer Edition\\firefox.exe",
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
-	
-	watch("./app/index.html", function() {
-		browserSync.reload();
-	});
+  
+  browserSync.init({
+    browser: "C:\\Program Files\\Firefox Developer Edition\\firefox.exe",
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+  
+  watch("./app/index.html", function() {
+    browserSync.reload();
+  });
 
-	watch("./app/assets/styles/**/*.css", function() {
-		gulp.start("cssInject");
-	});
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("cssInject");
+  });
+
+  watch("./app/assets/scripts/**/*.js", function() {
+    gulp.start("scriptsRefresh");
+  })
+
 });
 
-gulp.task("cssInject", ["styles"], function() {
-	return gulp.src("./app/temp/styles/styles.css")
-	.pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
+
+gulp.task("scriptsRefresh", ["scripts"], function() {
+  browserSync.reload();
+})
